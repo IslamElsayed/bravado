67f452b4c67ef721df50c6b6a6d6159367537cff
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2024-12-27 19:48:42 +0200

    Add ransackable_attributes

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,22 @@
+# frozen_string_literal: true
+
 class Car < ApplicationRecord
   belongs_to :brand
+
+  attr_accessor :rank_score, :label
+
+  validates :model, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+
+  # Define ransackable attributes
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[brand_id id model price]
+  end
+
+  # Define ransackable associations if needed
+  def self.ransackable_associations(_auth_object = nil)
+    ['brand']
+  end
+
+  # Additional validations and associations can be added here
 end
