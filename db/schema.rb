62a1fd6761622affa0e23b19fe52cbc0ab207798
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2024-12-27 19:48:58 +0200

    Add index to price in cars

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,43 +12,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_121316) do
-
+ActiveRecord::Schema.define(version: 20_241_223_130_335) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "brands", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'brands', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "cars", force: :cascade do |t|
-    t.string "model"
-    t.bigint "brand_id", null: false
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["brand_id"], name: "index_cars_on_brand_id"
+  create_table 'cars', force: :cascade do |t|
+    t.string 'model'
+    t.bigint 'brand_id', null: false
+    t.integer 'price'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['brand_id'], name: 'index_cars_on_brand_id'
+    t.index ['price'], name: 'index_cars_on_price'
   end
 
-  create_table "user_preferred_brands", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "brand_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["brand_id"], name: "index_user_preferred_brands_on_brand_id"
-    t.index ["user_id"], name: "index_user_preferred_brands_on_user_id"
+  create_table 'user_preferred_brands', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'brand_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['brand_id'], name: 'index_user_preferred_brands_on_brand_id'
+    t.index ['user_id'], name: 'index_user_preferred_brands_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.int8range "preferred_price_range"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'email'
+    t.int8range 'preferred_price_range'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "cars", "brands"
-  add_foreign_key "user_preferred_brands", "brands"
-  add_foreign_key "user_preferred_brands", "users"
+  add_foreign_key 'cars', 'brands'
+  add_foreign_key 'user_preferred_brands', 'brands'
+  add_foreign_key 'user_preferred_brands', 'users'
 end
